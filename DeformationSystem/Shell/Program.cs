7b0b531b01cc04a5bc3eca6cf9abcad9baca0e87
valtosw <volodijbessarabov@gmail.com>
2026-03-05 23:31:21 +0200

    Implement entry point

--- DeformationSystem/Shell/Program.cs
+++ DeformationSystem/Shell/Program.cs
@@ -1,11 +1,28 @@
-﻿namespace Shell
+﻿using Logging;
+using Microsoft.Extensions.Logging;
+using Visualization.UI;
+using Visualization.UI.Windows;
+
+namespace Shell
 {
-    internal static class Program
+    internal sealed class Program
     {
         [STAThread]
         private static void Main()
         {
-            throw new NotImplementedException();
+            var logger = Log.Create<Program>();
+
+            try
+            {
+                var application = new App();
+                var window = new MainWindow();
+
+                application.Run(window);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An unhandled exception occurred.");
+            }
         }
     }
 }
