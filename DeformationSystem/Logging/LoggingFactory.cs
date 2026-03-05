b4692226c597cb6e65ac82c4d15bf5682df0bdf4
valtosw <volodijbessarabov@gmail.com>
2026-03-05 23:29:44 +0200

    Add logging

---
+++ DeformationSystem/Logging/LoggingFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Logging
+{
+    internal static class LoggingFactory
+    {
+        private static ILoggerFactory? _loggerFactory;
+
+        public static ILoggerFactory Create()
+        {
+            if (_loggerFactory is not null)
+                return _loggerFactory;
+
+            _loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .SetMinimumLevel(LogLevel.Information)
+                    .AddConsole()
+                    .AddDebug();
+            });
+
+            return _loggerFactory;
+        }
+    }
+}
