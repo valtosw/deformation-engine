b5744d134d71099d13cd43968c7ce7562890d93b
valtosw <volodijbessarabov@gmail.com>
2026-03-06 22:12:12 +0200

    Add scene graph

---
+++ DeformationSystem/Visualization.Scene/Nodes/MeshNode.cs
@@ -0,0 +1,34 @@
+﻿using OpenTK.Mathematics;
+using Visualization.Abstractions.Geometry;
+
+namespace Visualization.Scene.Nodes
+{
+    public sealed class MeshNode : SceneNode
+    {
+        private Mesh? _mesh;
+
+        public Mesh? Mesh
+        {
+            get => _mesh;
+            set
+            {
+                if (ReferenceEquals(_mesh, value))
+                    return;
+
+                _mesh = value;
+                InvalidateBoundingBox();
+            }
+        }
+
+        public void NotifyGeometryChanged() => InvalidateBoundingBox();
+
+        protected override IEnumerable<Vector3> EnumerateLocalPoints()
+        {
+            if (_mesh is null)
+                yield break;
+
+            foreach (var vertex in _mesh.Vertices)
+                yield return vertex.Position;
+        }
+    }
+}
---
+++ DeformationSystem/Visualization.Scene/Nodes/SceneNode.cs
@@ -0,0 +1,133 @@
+﻿using OpenTK.Mathematics;
+using Visualization.Abstractions.Extensions;
+using Visualization.Abstractions.Geometry;
+
+namespace Visualization.Scene.Nodes
+{
+    public class SceneNode
+    {
+        private readonly List<SceneNode> _children = [];
+        private SceneNode? _parent;
+
+        private Vector3 _translation = Vector3.Zero;
+        private Quaternion _rotation = Quaternion.Identity;
+        private Vector3 _scale = Vector3.One;
+
+        private Matrix4? _cachedLocalTransform;
+        private Matrix4? _cachedWorldTransform;
+        private AxisAlignedBoundingBox? _cachedBoundingBox;
+
+        public SceneNode? Parent
+        {
+            get => _parent;
+            set
+            {
+                if (_parent == value)
+                    return;
+
+                _parent = value;
+                InvalidateWorldTransform();
+            }
+        }
+
+        public Vector3 Translation
+        {
+            get => _translation;
+            set
+            {
+                _translation = value;
+                InvalidateLocalTransform();
+            }
+        }
+
+        public Quaternion Rotation
+        {
+            get => _rotation;
+            set
+            {
+                _rotation = value;
+                InvalidateLocalTransform();
+            }
+        }
+
+        public Vector3 Scale
+        {
+            get => _scale;
+            set
+            {
+                _scale = value;
+                InvalidateLocalTransform();
+            }
+        }
+
+        public Matrix4 LocalTransform
+        {
+            get
+            {
+                _cachedLocalTransform ??= Matrix4.CreateScale(_scale) * Matrix4.CreateFromQuaternion(_rotation) * Matrix4.CreateTranslation(_translation);
+                return _cachedLocalTransform.Value;
+            }
+        }
+
+        public Matrix4 WorldTransform
+        {
+            get
+            {
+                _cachedWorldTransform ??= (Parent?.WorldTransform * LocalTransform) ?? LocalTransform;
+                return _cachedWorldTransform.Value;
+            }
+        }
+
+        public virtual AxisAlignedBoundingBox BoundingBox
+        {
+            get
+            {
+                if (_cachedBoundingBox is not null) 
+                    return _cachedBoundingBox;
+
+                var worldPoints = EnumerateLocalPoints().Select(point => WorldTransform.TransformPoint(point));
+                var boundingBox = AxisAlignedBoundingBox.FromPoints(worldPoints);
+                boundingBox = _children.Aggregate(boundingBox, (current, child) => AxisAlignedBoundingBox.Combine(current, child.BoundingBox));
+
+                return _cachedBoundingBox = boundingBox;
+            }
+        }
+
+        public void AddChild(SceneNode child)
+        {
+            child.Parent?.RemoveChild(child);
+
+            _children.Add(child);
+            child.Parent = this;
+        }
+
+        public void RemoveChild(SceneNode child)
+        {
+            if (_children.Remove(child))
+                child.Parent = null;
+        }
+
+        protected virtual IEnumerable<Vector3> EnumerateLocalPoints() => [];
+
+        private protected void InvalidateBoundingBox()
+        {
+            _cachedBoundingBox = null;
+            Parent?.InvalidateBoundingBox();
+        }
+
+        private void InvalidateLocalTransform()
+        {
+            _cachedLocalTransform = null;
+            InvalidateWorldTransform();
+        }
+
+        private void InvalidateWorldTransform()
+        {
+            _cachedWorldTransform = null;
+            InvalidateBoundingBox();
+
+            foreach (var child in _children)
+                child.InvalidateWorldTransform();
+        }
+    }
+}
