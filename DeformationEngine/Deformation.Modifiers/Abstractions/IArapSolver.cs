3ca3de6517f468f815fb7255ff078bddd92a0beb
valtosw <volodijbessarabov@gmail.com>
2026-06-01 19:20:56 +0300

    Add IArapSolver interface for ARAP deformation solvers

---
+++ DeformationEngine/Deformation.Modifiers/Abstractions/IArapSolver.cs
@@ -0,0 +1,16 @@
+﻿using Deformation.Abstractions.Geometry;
+using Deformation.Modifiers.Deformers.Arap;
+
+namespace Deformation.Modifiers.Abstractions
+{
+    public interface IArapSolver
+    {
+        bool IsUnavailable { get; }
+
+        void Clear();
+        void Invalidate();
+        bool TryPrepare(ArapSolverContext context);
+        void Solve(ArapSolverContext context);
+        void ApplyDeformation(Mesh mesh, ArapSolverContext context);
+    }
+}
