abb7a0e87081b90af3714bde90f16970bef881e5
valtosw <volodijbessarabov@gmail.com>
2026-06-01 19:22:07 +0300

    Add ArapSolverContext for ARAP solver state management

---
+++ DeformationEngine/Deformation.Modifiers/Deformers/Arap/ArapSolverContext.cs
@@ -0,0 +1,22 @@
+﻿using OpenTK.Mathematics;
+
+namespace Deformation.Modifiers.Deformers.Arap
+{
+    public sealed class ArapSolverContext
+    {
+        #region Properties
+
+        public Vector3[] OriginalPositions { get; init; } = [];
+        public Vector3[] WorkingPositions { get; init; } = [];
+        public Vector3[] ConstraintPositions { get; init; } = [];
+        public int[][] Neighbors { get; init; } = [];
+        public bool[] Constrained { get; init; } = [];
+        public int ConstraintVersion { get; init; }
+        public int Iterations { get; init; }
+        public bool UseIdentityRotations { get; init; }
+        public IReadOnlySet<int> ControlVertices { get; init; } = new HashSet<int>();
+        public Func<Vector3, Vector3> TransformControlPoint { get; init; } = _ => Vector3.Zero;
+
+        #endregion
+    }
+}
